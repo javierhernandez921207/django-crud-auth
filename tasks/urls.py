848fa399160eb94313b646dbe75ed0f737cda525
Javier Hernandez <javier.hernandez@ntsprint.com>
2024-02-23 15:44:18 -0500

    Add signup feature

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world')
+    path('', views.home, name='index'),
+    path('signup/', views.signup, name='signup'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
-def hello_world(request):
-    return HttpResponse('hello_world')
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm()})
+    else:
+        # POST, create a new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created')
+            except:
+                return HttpResponse('User already exists')           
+        else:
+            return HttpResponse('Passwords did not match')
